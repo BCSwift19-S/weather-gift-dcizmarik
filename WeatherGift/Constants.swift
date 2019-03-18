bc90c80d06cf9631bf127833439615838a2f0e63
Daniel Cizmarik <dcizmarik@daniels-macbook-pro.local>
2019-03-18 12:01:01 -0400

    Completed 7.15

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Daniel Cizmarik on 3/18/19.
+//  Copyright © 2019 Daniel Cizmarik. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "7b36e4737d1d572a697f787cfb2d5b1e/"
